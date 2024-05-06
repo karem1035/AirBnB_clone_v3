33cd5a86d4037d98de7902003b9b123a72cb4891
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-05-06 03:34:23 +0300

    task 1

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+import uuid
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from os import environ
-from flask import Flask, render_template
-import uuid
 app = Flask(__name__)
 
 
@@ -17,7 +16,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/100-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -33,7 +32,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
